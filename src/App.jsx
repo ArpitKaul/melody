aa29e82f4f9fb69e85dd257e155498b10fb7727c
ArpitKaul <akaul7407@gmail.com>
2024-08-04 20:28:41 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import Sidebar from './Components/Sidebar'
-import Player from './Components/Player'
-import Display from './Components/Display'
+import React, { useContext } from 'react';
+import Sidebar from './Components/Sidebar';
+import Player from './Components/Player';
+import Display from './Components/Display';
+import { playerContext } from './Context/PlayerContext';
 
 const App = () => {
+  const { audioRef , track  } = useContext(playerContext);
+
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
-        <Sidebar/>
-        <Display/> 
+        <Sidebar />
+        <Display />
       </div>
-      <Player/>
+      <Player />
+      <audio ref={audioRef} src={track.file}preload='auto'></audio>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { assets } from '../assets/assets';
+import { useNavigate } from 'react-router-dom';
 
 const Sidebar = () => {
+
+  const navigate = useNavigate();
   return (
     <div className='w-[25%] h-full p-2 flex-col text-white hidden lg:flex'>
       <div className='bg-[#121212] h-[6rem] rounded flex flex-col justify-around mb-2'>
-        <div className='flex items-center gap-3 pl-8 cursor-pointer'>
+        <div onClick={()=>navigate('/')} className='flex items-center gap-3 pl-8 cursor-pointer'>
           <img className="w-6" src={assets.home_icon} alt="home icon" />
           <p className='font-bold'>Home</p>
         </div>
@@ -27,7 +30,7 @@ const Sidebar = () => {
         </div>
         <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
           <h1>Create Your First Playlist</h1>
-          <p className='font-light '>it's easy we will help you</p>
+          <p className='font-light  '>it's easy we will help you</p>
           <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Create PlayList</button>
         </div>
         <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4'>
